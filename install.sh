083557b5b65c97c137871956eb8eb8ebdcbb0030
Akmal <akmalthemalong@gmail.com>
2023-11-12 21:02:20 +0800

    add: install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+echo "Installing Packages"
+echo "Installing ZSH..."
+pkg install zsh
+
+echo
+echo "Installing Neovim..."
+pkg install neovim
+
+echo "Installing GNU Stow"
+pkg install stow
+
+echo
+echo "Installing Tmux..."
+pkg install tmux
+
+echo
+echo "Installing Pyhton..."
+pkg install python
+
+echo
+echo "Installing Exa"
+pkg install exa
